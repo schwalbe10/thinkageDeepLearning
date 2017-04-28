c743f8c9491bdef51b9cd806ca799a2e01c44fbc
schwalbe10 <schwalbe10@gmail.com>
2017-04-28 21:54:42 +0900

    Add 05_batch.py

---
+++ 05_batch.py
@@ -0,0 +1,40 @@
+import os
+import numpy as np
+import pickle
+from mnist import load_mnist
+from functions import sigmoid, softmax
+
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
+
+def init_network():
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    l = sigmoid(np.dot(x, W1) + b1)
+    m = sigmoid(np.dot(l, W2) + b2)
+    y = softmax(np.dot(m, W3) + b3)
+
+    return y
+
+
+x, t = get_data()
+network = init_network()
+
+batch_size = 100
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
